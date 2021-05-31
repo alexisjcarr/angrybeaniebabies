d02414ccecdc596577907eb486b96574945c130d
alexis.carr <alexis.carr@eventbrite.com>
2021-05-31 12:31:30 -0500

    refactor

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ BULLET_VEL = 7
 MAX_BULLETS = 3
 BEAR_WIDTH, BEAR_HEIGHT = 55, 40
 
-YELLOW_HIT = pygame.USEREVENT + 1
-RED_HIT = pygame.USEREVENT + 2
+LEFT_BEAR_HIT = pygame.USEREVENT + 1
+RIGHT_BEAR_HIT = pygame.USEREVENT + 2
 
 LEFT_BEAR = pygame.image.load(
     os.path.join('Assets', 'Teddy_32x32_sand_heart_stand_R.png'))
@@ -45,66 +45,66 @@ BACKGROUND = pygame.transform.scale(pygame.image.load(
 
 class Game:
     def _draw_window(
-        self, red, yellow, red_bullets,
-        yellow_bullets, red_health, yellow_health
+        self, right_bear, left_bear, right_bear_bullets,
+        left_bear_bullets, right_bear_health, left_bear_health
     ):
         WIN.blit(BACKGROUND, (0, 0))
         pygame.draw.rect(WIN, BLACK, BORDER)
 
-        red_health_text = HEALTH_FONT.render(
-            "Health: " + str(red_health), 1, WHITE)
-        yellow_health_text = HEALTH_FONT.render(
-            "Health: " + str(yellow_health), 1, WHITE)
-        WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
-        WIN.blit(yellow_health_text, (10, 10))
+        right_bear_health_text = HEALTH_FONT.render(
+            "Health: " + str(right_bear_health), 1, WHITE)
+        left_bear_health_text = HEALTH_FONT.render(
+            "Health: " + str(left_bear_health), 1, WHITE)
+        WIN.blit(right_bear_health_text, (WIDTH - right_bear_health_text.get_width() - 10, 10))
+        WIN.blit(left_bear_health_text, (10, 10))
 
-        WIN.blit(LEFT_BEAR, (yellow.x, yellow.y))
-        WIN.blit(RIGHT_BEAR, (red.x, red.y))
+        WIN.blit(LEFT_BEAR, (left_bear.x, left_bear.y))
+        WIN.blit(RIGHT_BEAR, (right_bear.x, right_bear.y))
 
-        for bullet in red_bullets:
+        for bullet in right_bear_bullets:
             pygame.draw.rect(WIN, RED, bullet)
 
-        for bullet in yellow_bullets:
+        for bullet in left_bear_bullets:
             pygame.draw.rect(WIN, YELLOW, bullet)
 
         pygame.display.update()
 
-    def _yellow_handle_movement(self, keys_pressed, yellow):
-        if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
-            yellow.x -= VEL
-        if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # RIGHT
-            yellow.x += VEL
-        if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
-            yellow.y -= VEL
-        if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:  # DOWN
-            yellow.y += VEL
+    def _left_bear_handle_movement(self, keys_pressed, left_bear_bear):
+        if keys_pressed[pygame.K_a] and left_bear_bear.x - VEL > 0:  # LEFT
+            left_bear_bear.x -= VEL
+        if keys_pressed[pygame.K_d] and left_bear_bear.x + VEL + left_bear_bear.width < BORDER.x:  # RIGHT
+            left_bear_bear.x += VEL
+        if keys_pressed[pygame.K_w] and left_bear_bear.y - VEL > 0:  # UP
+            left_bear_bear.y -= VEL
+        if keys_pressed[pygame.K_s] and left_bear_bear.y + VEL + left_bear_bear.height < HEIGHT - 15:  # DOWN
+            left_bear_bear.y += VEL
 
-    def _red_handle_movement(self, keys_pressed, red):
-        if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  # LEFT
-            red.x -= VEL
-        if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:  # RIGHT
-            red.x += VEL
-        if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # UP
-            red.y -= VEL
-        if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:  # DOWN
-            red.y += VEL
+    def _right_bear_handle_movement(self, keys_pressed, right_bear_bear):
+        if keys_pressed[pygame.K_LEFT] and right_bear_bear.x - VEL > BORDER.x + BORDER.width:  # LEFT
+            right_bear_bear.x -= VEL
+        if keys_pressed[pygame.K_RIGHT] and right_bear_bear.x + VEL + right_bear_bear.width < WIDTH:  # RIGHT
+            right_bear_bear.x += VEL
+        if keys_pressed[pygame.K_UP] and right_bear_bear.y - VEL > 0:  # UP
+            right_bear_bear.y -= VEL
+        if keys_pressed[pygame.K_DOWN] and right_bear_bear.y + VEL + right_bear_bear.height < HEIGHT - 15:  # DOWN
+            right_bear_bear.y += VEL
 
-    def _handle_bullets(self, yellow_bullets, red_bullets, yellow, red):
-        for bullet in yellow_bullets:
+    def _handle_bullets(self, left_bear_bullets, right_bear_bullets, left_bear, right_bear):
+        for bullet in left_bear_bullets:
             bullet.x += BULLET_VEL
-            if red.colliderect(bullet):
-                pygame.event.post(pygame.event.Event(RED_HIT))
-                yellow_bullets.remove(bullet)
+            if right_bear.colliderect(bullet):
+                pygame.event.post(pygame.event.Event(RIGHT_BEAR_HIT))
+                left_bear_bullets.remove(bullet)
             elif bullet.x > WIDTH:
-                yellow_bullets.remove(bullet)
+                left_bear_bullets.remove(bullet)
 
-        for bullet in red_bullets:
+        for bullet in right_bear_bullets:
             bullet.x -= BULLET_VEL
-            if yellow.colliderect(bullet):
-                pygame.event.post(pygame.event.Event(YELLOW_HIT))
-                red_bullets.remove(bullet)
+            if left_bear.colliderect(bullet):
+                pygame.event.post(pygame.event.Event(LEFT_BEAR_HIT))
+                right_bear_bullets.remove(bullet)
             elif bullet.x < 0:
-                red_bullets.remove(bullet)
+                right_bear_bullets.remove(bullet)
 
     def _draw_winner(self, text):
         WINNER.play()
@@ -112,18 +112,18 @@ class Game:
         WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
                             2, HEIGHT/2 - draw_text.get_height()/2))
         pygame.display.update()
-        pygame.time.delay(5000)
+        pygame.time.delay(10000)
 
     def main(self):
         OPENER.play()
-        red = pygame.Rect(700, 300, BEAR_WIDTH, BEAR_HEIGHT)
-        yellow = pygame.Rect(100, 300, BEAR_WIDTH, BEAR_HEIGHT)
+        right_bear = pygame.Rect(700, 300, BEAR_WIDTH, BEAR_HEIGHT)
+        left_bear = pygame.Rect(100, 300, BEAR_WIDTH, BEAR_HEIGHT)
 
-        red_bullets = []
-        yellow_bullets = []
+        right_bear_bullets = []
+        left_bear_bullets = []
 
-        red_health = 10
-        yellow_health = 10
+        right_bear_health = 10
+        left_bear_health = 10
 
         clock = pygame.time.Clock()
         run = True
@@ -135,45 +135,45 @@ class Game:
                     pygame.quit()
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    if event.key == pygame.K_LCTRL and len(left_bear_bullets) < MAX_BULLETS:
                         bullet = pygame.Rect(
-                            yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
-                        yellow_bullets.append(bullet)
+                            left_bear.x + left_bear.width, left_bear.y + left_bear.height//2 - 2, 10, 5)
+                        left_bear_bullets.append(bullet)
                         BULLET_FIRE_SOUND.play()
 
-                    if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    if event.key == pygame.K_RCTRL and len(right_bear_bullets) < MAX_BULLETS:
                         bullet = pygame.Rect(
-                            red.x, red.y + red.height//2 - 2, 10, 5)
-                        red_bullets.append(bullet)
+                            right_bear.x, right_bear.y + right_bear.height//2 - 2, 10, 5)
+                        right_bear_bullets.append(bullet)
                         BULLET_FIRE_SOUND.play()
 
-                if event.type == RED_HIT:
-                    red_health -= 1
+                if event.type == RIGHT_BEAR_HIT:
+                    right_bear_health -= 1
                     BULLET_HIT_SOUND.play()
 
-                if event.type == YELLOW_HIT:
-                    yellow_health -= 1
+                if event.type == LEFT_BEAR_HIT:
+                    left_bear_health -= 1
                     BULLET_HIT_SOUND.play()
 
             winner_text = ""
-            if red_health <= 0:
-                winner_text = "Yellow Wins!"
+            if right_bear_health <= 0:
+                winner_text = "The Bear on the Left Wins!"
 
-            if yellow_health <= 0:
-                winner_text = "Red Wins!"
+            if left_bear_health <= 0:
+                winner_text = "The Bear on the Right Wins!"
 
             if winner_text != "":
                 self._draw_winner(winner_text)
                 break
 
             keys_pressed = pygame.key.get_pressed()
-            self._yellow_handle_movement(keys_pressed, yellow)
-            self._red_handle_movement(keys_pressed, red)
+            self._left_bear_handle_movement(keys_pressed, left_bear)
+            self._right_bear_handle_movement(keys_pressed, right_bear)
 
-            self._handle_bullets(yellow_bullets, red_bullets, yellow, red)
+            self._handle_bullets(left_bear_bullets, right_bear_bullets, left_bear, right_bear)
 
-            self._draw_window(red, yellow, red_bullets, yellow_bullets,
-                              red_health, yellow_health)
+            self._draw_window(right_bear, left_bear, right_bear_bullets, left_bear_bullets,
+                              right_bear_health, left_bear_health)
 
         self.main()
 
